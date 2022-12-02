e4197b802f86d2f02145db0153c491ecb44fee1b
2024davidchen <2024davidchen@gmail.com>
2022-12-01 23:31:48 -0500

    Finished Day 1

--- day1-puzzle1.py
+++ day1-puzzle1.py
@@ -1,1 +1,18 @@
+# Part 1
 elves = []
+inputFile = open("day1-puzzle1-input.txt", "r")
+tempElfCalories = 0
+for line in inputFile:
+    if (line != "\n"):
+        tempElfCalories += int(line)
+    else:
+        elves.append(tempElfCalories)
+        tempElfCalories = 0
+print(max(elves))
+
+# Part 2
+topThree = 0
+for i in range(3):
+    topThree += max(elves)
+    elves.remove(max(elves))
+print("The top three total to: " + str(topThree))
