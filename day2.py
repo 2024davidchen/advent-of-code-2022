710a343a19228fb92d59cde819c7b2cbce624ebf
2024davidchen <2024davidchen@gmail.com>
2022-12-02 16:26:05 -0500

    Solved Day 2

--- day2.py
+++ day2.py
@@ -1,13 +1,44 @@
+# Part 1
 inputFile = open("day2-input.txt", "r")
 points = 0
 for line in inputFile:
-  if (line(2) == 'X'):
+  if (line[2] == 'X'):
     points += 1
-  if (line(2) == 'Y'):
+  if (line[2] == 'Y'):
     points += 2
-  if (line(2) == 'Z'):
+  if (line[2] == 'Z'):
     points += 3
-  if (line == "A Y" || line == "B Z" || line == "C X"):
+  if ((line[0] == 'A' and line[2] == 'Y') or (line[0] == 'B' and line[2] == 'Z') or (line[0] == 'C' and line[2] == 'X')):
     points += 6
-  if (line == "A X" || line == "B Y" || line == "C Z"):
+  if ((line[0] == 'A' and line[2] == 'X') or (line[0] == 'B' and line[2] == 'Y') or (line[0] == 'C' and line[2] == 'Z')):
     points += 3
+print(points)
+
+# Part 2
+inputFile = open("day2-input.txt", "r")
+points = 0
+for line in inputFile:
+  if (line[2] == 'X'):
+    if (line[0] == 'A'):
+      points += 3
+    if (line[0] == 'B'):
+      points += 1
+    if (line[0] == 'C'):
+      points += 2
+  if (line[2] == 'Y'):
+    points += 3
+    if (line[0] == 'A'):
+      points += 1
+    if (line[0] == 'B'):
+      points += 2
+    if (line[0] == 'C'):
+      points += 3
+  if (line[2] == 'Z'):
+    points += 6
+    if (line[0] == 'A'):
+      points += 2
+    if (line[0] == 'B'):
+      points += 3
+    if (line[0] == 'C'):
+      points += 1
+print(points)
