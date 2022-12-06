6f39d41d040d7029288fc45719f97b81319fb785
2024davidchen <84689232+2024davidchen@users.noreply.github.com>
2022-12-06 09:05:20 -0500

    Rearranged Lines

--- day6-part1.py
+++ day6-part1.py
@@ -1,13 +1,6 @@
 inputFile = open("day6-input.txt", "r")
 inputString = inputFile.readLines()[0]
 
-i = #
-boolean noRepeats = False
-while (not noRepeats):
-  i += 1
-  packet = inputString[i - 4 : i]
-  noRepeats = not checkRepeats(packet)
-
 def checkRepeats(string):
   result = false
   for x in string:
@@ -15,5 +8,12 @@ def checkRepeats(string):
       result = true
       break
   return result
+
+i = 3
+noRepeats = False
+while (not noRepeats):
+  i += 1
+  packet = inputString[i - 4 : i]
+  noRepeats = not checkRepeats(packet)
 
 print(i)
