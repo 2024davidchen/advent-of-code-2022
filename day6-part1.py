9e315cb72fb70b7680f6304b517e842f42296c12
2024davidchen <84689232+2024davidchen@users.noreply.github.com>
2022-12-06 08:51:33 -0500

    Update day6-part1.py

--- day6-part1.py
+++ day6-part1.py
@@ -1,11 +1,11 @@
 inputFile = open("day6-input.txt", "r")
 inputString = inputFile.readLines()[0]
 
-i = 4
+i = #
 boolean noRepeats = False
 while (not noRepeats):
-  packet = inputString[i - 4 : i]
   i += 1
+  packet = inputString[i - 4 : i]
   noRepeats = not checkRepeats(packet)
 
 def checkRepeats(string):
